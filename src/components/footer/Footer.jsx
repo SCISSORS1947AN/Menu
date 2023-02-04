0090e0a7fd84c187c5d7f8deeabb23bf66999bbc
Thamizhiniyan C S <csthamizhiniyan@gmail.com>
2023-02-05 00:30:09 +0530

    v1.0.1

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import styles from "./Footer.module.css";
+import instagram from "../../assets/instagram.png";
+import whatsapp from "../../assets/whatsapp.png";
+import google from "../../assets/google.png";
+
+const Footer = () => {
+  return (
+    <div className={styles.Footer}>
+      <div className={styles.offer}>
+        <h2>To Avail EXTRA 10% OFF...!!!</h2>
+        <a href="https://www.instagram.com/scissors_1947/">
+          <button>
+            <img src={instagram} alt="" />
+            Follow Us
+          </button>
+        </a>
+        <a href="https://g.page/r/CQcsqlWiTNE7EB0/review">
+          <button>
+            <img src={google} alt="" />
+            Rate Us!!!
+          </button>
+        </a>
+      </div>
+      <div className={styles.socials}>
+        <div>
+          <a href="https://www.instagram.com/scissors_1947/">
+            <img src={instagram} alt="" />
+          </a>
+        </div>
+        <div>
+          <a href="https://api.whatsapp.com/send?phone=917598760506">
+            <img src={whatsapp} alt="" />
+          </a>
+        </div>
+      </div>
+      <p>
+        <span>Developed by</span>{" "}
+        <a href="https://linktr.ee/thamizhiniyancs">Thamizhiniyan C S</a> &#169;
+        2023
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
